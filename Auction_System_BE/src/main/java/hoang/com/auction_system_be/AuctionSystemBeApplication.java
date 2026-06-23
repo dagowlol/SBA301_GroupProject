e47ea8531a8671607652f6ba0cef4820b4e94c29
QuanEnti <quansieunhoc@gmail.com>
2026-06-23 09:27:40 +0700

    chore: update dependencies and config

--- Auction_System_BE/src/main/java/hoang/com/auction_system_be/AuctionSystemBeApplication.java
+++ Auction_System_BE/src/main/java/hoang/com/auction_system_be/AuctionSystemBeApplication.java
@@ -2,9 +2,11 @@ package hoang.com.auction_system_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableCaching
 @EnableScheduling
 @SpringBootApplication
 @EnableAsync
