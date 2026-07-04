bd0064cba2b35d50114c057deba399bcd49a1d57
Huwng18 <hungthanh18122005@gmail.com>
2026-07-04 23:01:42 +0700

    fix: move AboutUsPage.css to folder style

--- Auction_System_FE/src/features/catalog/pages/AboutUsPage.jsx
+++ Auction_System_FE/src/features/catalog/pages/AboutUsPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import './AboutUsPage.css';
+import '../styles/AboutUsPage.css';
 
 export default function AboutUsPage() {
   return (
